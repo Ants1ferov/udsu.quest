b15ffc4364262d283559e72158d06d777b051af1
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-18 23:49:33 +0400

    create main.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import auth from "@/pages/auth.vue";
 import scanner from "@/pages/scanner.vue";
 import help from "@/pages/help.vue";
 import map from "@/pages/map.vue";
+import main from "@/pages/main.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'main',
+      component: main
+    },
+    {
+      path: '/auth',
       name: 'auth',
       component: auth
     },
