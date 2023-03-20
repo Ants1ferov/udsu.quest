7fd322c111cc1dc8ff5058e015b9686eea3b62a9
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-21 02:14:27 +0400

    update maps

---
+++ src/components/UI/yandex-maps.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ya-img">
+    <div id="map" style="width: 100%; height: 400px"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "yandex-maps",
+  mounted() {
+    ymaps.ready(init);
+    function init(){
+      var myMap = new ymaps.Map("map", {
+        center: [56.844301,53.212378],
+        zoom: 16
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .ya-img {
+  }
+</style>
--- src/pages/map.vue
+++ src/pages/map.vue
@@ -1,21 +1,80 @@
 <template>
-  <div class="map-block">
-    <iframe class="ya-map" src="https://yandex.ru/map-widget/v1/?um=constructor%3A6b70dc865bbde0245160718afaabdda239ae5f43006246da27ace42e303ac0a1&amp;source=constructor"></iframe>
+  <AppHeader></AppHeader>
+  <div class="map" id="map-container">
+    <yandex-maps></yandex-maps>
+    <div class="block-btn">
+      <AppButton @click="changeMap1" class="dark bold btn-dark m15">1</AppButton>
+      <AppButton @click="changeMap2" class="dark bold btn-dark m15">2</AppButton>
+      <AppButton @click="changeMap3" class="dark bold btn-dark m15">3</AppButton>
+      <AppButton @click="changeMap4" class="dark bold btn-dark m15">4</AppButton>
+      <AppButton @click="changeMap5" class="dark bold btn-dark m15">5</AppButton>
+      <AppButton @click="changeMap6" class="dark bold btn-dark m15">6</AppButton>
+      <AppButton @click="changeMap7" class="dark bold btn-dark m15">7</AppButton>
+    </div>
   </div>
 </template>
 
 <script>
 
+import AppHeader from "@/components/UI/AppHeader.vue";
+import YandexMaps from "@/components/UI/yandex-maps.vue";
+import AppButton from "@/components/UI/AppButton.vue";
+
 export default {
-  name: "map"
+  name: "map",
+  components: {AppButton, YandexMaps, AppHeader},
+  data() {
+    return {
+      map: {
+        map1: true,
+        map2: false,
+        map3: false,
+        map4: false,
+        map5: false,
+        map6: false,
+        map7: false
+      }
+    }
+  },
+  methods: {
+    changeMap1() {
+
+    },
+    changeMap2() {
+
+    },
+    changeMap3() {
+
+    },
+    changeMap4() {
+
+    },
+    changeMap5() {
+
+    },
+    changeMap6() {
+
+    },
+    changeMap7() {
+
+    }
+  }
 }
+
 </script>
 
 <style scoped>
-  .map-block {
+  .map {
     padding-top: 70px;
     width: 100%;
-    height: 85vh;
+    height: 40vh;
+  }
+  .block-btn {
+    display: flex;
+    margin: 0 auto;
+    width: 70%;
+    justify-content: center;
+    flex-wrap: wrap;
   }
   .ya-map {
     width: 100%;
