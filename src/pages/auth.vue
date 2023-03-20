f557c3bd271ebc940288932e3f4e0f55034526da
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-20 14:18:15 +0400

    update auth

--- src/pages/auth.vue
+++ src/pages/auth.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="auth">
+    <div class="logo-block">
+      <img class="logo-img" src="./../assets/img/design/logo.svg">
+      <p class="logo-title">
+        ИЖТРУДФРОНТ
+        <img class="logo-cycle" src="./../assets/img/design/cycle.svg">
+        ИЖТРУДФРОНТ
+      </p>
+    </div>
     <div class="auth-block">
       <form @submit.prevent="registration" class="auth-form">
         <transition name="ok">
-          <div class="sky-block" v-if="registerOk">
+          <sky-block v-if="registerOk">
             <div class="register-ok-icon">
-              <img class="register-ok-icon-img" src="@/assets/img/button/ok.svg">
+              <svg xmlns="http://www.w3.org/2000/svg" height="128" width="128" viewBox="0 0 48 48">
+                <path d="M18.9 35.1q-.3 0-.55-.1-.25-.1-.5-.35L8.8 25.6q-.45-.45-.45-1.1 0-.65.45-1.1.45-.45 1.05-.45.6 0 1.05.45l8 8 18.15-18.15q.45-.45 1.075-.45t1.075.45q.45.45.45 1.075T39.2 15.4L19.95 34.65q-.25.25-.5.35-.25.1-.55.1Z" fill="#adff00"/>
+              </svg>
             </div>
-          </div>
+          </sky-block>
+        </transition>
+        <transition name="ok">
+          <sky-block v-if="registerFail">
+            <div class="register-ok-icon">
+              <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M64 69.6L36 97.6C35.2 98.4 34.2667 98.8 33.2 98.8C32.1333 98.8 31.2 98.4 30.4 97.6C29.6 96.8 29.2 95.8667 29.2 94.8C29.2 93.7333 29.6 92.8 30.4 92L58.4 64L30.4 36C29.6 35.2 29.2 34.2667 29.2 33.2C29.2 32.1333 29.6 31.2 30.4 30.4C31.2 29.6 32.1333 29.2 33.2 29.2C34.2667 29.2 35.2 29.6 36 30.4L64 58.4L92 30.4C92.8 29.6 93.7333 29.2 94.8 29.2C95.8667 29.2 96.8 29.6 97.6 30.4C98.4 31.2 98.8 32.1333 98.8 33.2C98.8 34.2667 98.4 35.2 97.6 36L69.6 64L97.6 92C98.4 92.8 98.8 93.7333 98.8 94.8C98.8 95.8667 98.4 96.8 97.6 97.6C96.8 98.4 95.8667 98.8 94.8 98.8C93.7333 98.8 92.8 98.4 92 97.6L64 69.6Z" fill="#D3474C"/>
+              </svg>
+              <p class="reg-error-text">Ошибка</p>
+              <AppButton class="close-block" type="button" @click="closeBlock">Закрыть</AppButton>
+            </div>
+          </sky-block>
         </transition>
         <input type="text" class="form-input" autocomplete="name" id="name" placeholder="Имя" v-model="name">
         <input type="text" class="form-input" autocomplete="family-name" id="surname" placeholder="Фамилия" v-model="surname">
@@ -20,18 +41,18 @@
       <div class="auth-block-button-recovery">
         <button class="recovery-button" @click="recoveryOpenBlock">Забыли пароль?</button>
         <transition name="ok">
-          <div class="sky-block" v-if="recoveryOpen">
+          <sky-block v-if="recoveryOpen">
             <form @submit.prevent="checkValue" class="recovery-form">
               <div class="recovery-block-title">Восстановление пароля</div>
+              <div class="captcha-title">Введите свой email:</div>
               <input type="email" class="form-input" autocomplete="email" id="email" placeholder="Email" :class="{ emailVoid: emailVoid }" v-model="email">
               <div class="captcha">
-                <div class="captcha-title">Введите капчу:</div>
-                <div class="captcha-value">{{ captchaValue }}</div>
-                <input type="text" class="form-input captcha" placeholder="Капча" :class="{ captchaVoid: captchaVoid }" v-model="captchaUserValue">
+                <div class="captcha-title">Введите новый пароль:</div>
+                <input type="text" class="form-input" placeholder="Новый пароль" :class="{newPasswordVoid: newPasswordVoid}" v-model="newPassword">
                 <AppButton class="recovery-button-end">Сбросить пароль</AppButton>
               </div>
             </form>
-          </div>
+          </sky-block>
         </transition>
       </div>
     </div>
@@ -42,99 +63,129 @@
 import AppButton from "@/components/UI/AppButton.vue";
 import axios from "axios";
 import App from "@/App.vue";
+import SkyBlock from "@/components/UI/sky-block.vue";
 
 export default {
   name: "auth",
-  components: {App, AppButton},
+  components: {SkyBlock, App, AppButton},
   data() {
-    let captchaValue = '';
-    let captchaUserValue = '';
     return {
+      newPassword: '',
       recoveryOpen: false,
       registerOk: false,
+      registerFail: false,
       name: '',
       surname: '',
       email: '',
       code: '',
       password: '',
-      captchaUserValue,
-      captchaValue,
-      captchaVoid: false,
+      newPasswordVoid: false,
       emailVoid: false
     }
   },
   mounted() {
   },
   methods: {
-    successful() {
-      this.registerOk = this.registerOk === false;
-/*
-      this.registerOk = true
-*/
-    },
     registration() {
-      this.successful()
-      axios
-          .post('https://api.udgu.suslovd.ru:9443/api/add', {
-            firstname: this.name,
-            secondname: this.surname,
-            email: this.email,
-            password: this.password,
-          })
-      setTimeout(() => {
-        this.$router.push('/safety-rules')
-      }, 3000);
+      if (this.name !== '' && this.surname !== '' && this.email !== '' && this.password !== '') {
+        axios
+            .post('https://api.udgu.suslovd.ru:9443/api/add', {
+              firstname: this.name,
+              secondname: this.surname,
+              email: this.email,
+              password: this.password,
+            })
+            .then((response) => {
+              this.registerOk = true
+              console.log(response.data)
+              setTimeout(() => {
+                this.$router.push('/safety-rules')
+              }, 1000);
+            })
+            .catch((reason) => {
+              this.registerFail = true
+            })
+      } else {
+        this.registerFail = true
+      }
     },
     recoveryOpenBlock() {
-      this.captchaValue = Math.floor(Math.random() * (9999 - 1000) + 1000);
-      this.recoveryOpen = true
+      if (this.recoveryOpen === false) {
+        this.recoveryOpen = true
+      } else {
+        this.recoveryOpen = false
+      }
+    },
+    closeBlock() {
+      if (this.registerFail === true) {
+        this.registerFail = false
+      } else {
+        this.registerFail = true
+      }
     },
     recoveryOk() {
       console.log('Пароль сброшен')
     },
     checkValue() {
-      if (this.captchaUserValue === '' && this.email === '') {
-        this.captchaVoid = true
+      if (this.newPassword === '' && this.email === '') {
+        this.newPasswordVoid = true
         this.emailVoid = true
-        console.log('Введите капчу и email')
-      }
-      else if (this.captchaUserValue === '') {
-        this.captchaVoid = true
-        console.log('Введите капчу')
-      }
-      else if (this.email === '') {
+        console.log('Введите новый пароль и email')
+      } else if (this.newPassword === '') {
+        this.newPasswordVoid = true
+        console.log('Введите новый пароль')
+      } else if (this.email === '') {
         this.emailVoid = true
-        console.log('Введите email!')
+        console.log('Введите email')
+      } else {
+        axios
+            .post('https://api.udgu.suslovd.ru:9443/api/remind', {
+              email: this.email,
+              password: this.newPassword
+            })
+            .then((response) => {
+              this.recoveryOpen = false
+              this.recoveryOk()
+              console.log(response.data)
+            })
+            .catch((reason) => {
+              console.log(reason)
+            })
       }
-      else {
-        if (this.captchaUserValue == this.captchaValue) {
-          console.log('Капча правильная')
-          axios
-              .post('https://api.udgu.suslovd.ru:9443/api/change', {
-                email: this.email
-              }).
-              then(() => {
-                this.recoveryOk()
-              })
-              .catch(() => {
-
-              })
-        } else {
-          console.log('false')
-        }
-      }
-    },
+    }
   }
 }
 </script>
 <style>
+.logo-block {
+  display: flex;
+  flex-direction: column;
+  padding-top: 30px;
+  align-items: center
+}
+.logo-img {
+  width: 256px;
+  height: 260px;
+}
+.logo-title {
+  margin-top: 35px;
+  text-align: center;
+  font-size: 20px;
+  color: black;
+  font-weight: bolder;
+}
+.logo-cycle {
+  position: relative;
+  top: 5px;
+  width: 24px;
+}
   .auth {
     height: 100vh;
     width: 100%;
     background-color: white
   }
   .auth-block {
-    padding: 175px 35px;
+    padding: 50px 35px;
   }
   .auth-form {
     display: flex;
@@ -143,20 +194,7 @@ export default {
     min-width: 200px;
     max-width: 500px;
   }
-  .sky-block {
-    display: flex;
-    position: absolute;
-    width: 80%;
-    height: 60%;
-    margin: auto;
-    top: 0; left: 0; bottom: 0; right: 0;
-    text-align: center;
-    background-color: rgba(0, 0, 0, 0.50);
-    backdrop-filter: blur(10px);
-    z-index: 2;
-    border-radius: 25px;
-    align-items: center;
-  }
+
   .register-ok-icon {
     margin: 0 auto;
   }
@@ -168,18 +206,24 @@ export default {
     transition: all 750ms cubic-bezier(0, 1, .15, 1);
   }
   .ok-leave-active {
-    transition: background-color 400ms ease-out;
+    transition: all 1500ms cubic-bezier(0, 1, .15, 1);
   }
-  .ok-enter-from,
-  .ok-leave-to {
+  .ok-enter-from {
     opacity: 1;
     transform: translateY(-150%);
+  }
+  .ok-leave-to {
+    opacity: 0;
+    transform: translateY(+150%);
+  }
+  .reg-error-text {
+    font-size: 20px;
+    color: white;
   }
   .form-input {
     background: #e3e5e3;
     color: #7a7c7a;
     border: 0;
-    font-family: Inter, sans-serif;
     padding: 5px 20px;
     font-size: 20px;
     border-radius: 17px;
@@ -191,6 +235,10 @@ export default {
   .reg-btn {
     margin: 10px auto auto 0;
     float: right;
+  }
+  .close-block {
+    font-size: 20px;
+    margin-top: 35px;
   }
   .auth-block-button-recovery {
     margin: 100px auto;
@@ -214,18 +262,20 @@ export default {
     color: red;
   }
   .recovery-block-title {
-    font-size: 24px;
+    font-size: 32px;
     color: #ffffff;
+    margin-bottom: 35px;
   }
   .captcha {
 
   }
-  .form-input.captcha.captchaVoid::placeholder {
+  .form-input.newPasswordVoid::placeholder {
     color: red;
   }
   .captcha-title {
     color: white;
     font-size: 18px;
+    text-align: left;
   }
   .form-input.captcha {
     width: 50%;
@@ -235,7 +285,7 @@ export default {
     color: #00ff15;
   }
   .recovery-button-end {
-    margin: 10px 0;
+    margin: 25px 0;
     color: white;
     font-size: 20px;
   }
@@ -265,7 +315,7 @@ export default {
       font-size: 44px;
     }
     .auth-block {
-      padding: 175px 25px;
+      padding: 35px 25px;
     }
   }
   @media only screen and (max-width: 350px) {
@@ -273,7 +323,7 @@ export default {
       font-size: 40px;
     }
     .auth-block {
-      padding: 175px 15px;
+      padding: 25px 15px;
     }
   }
 </style>
