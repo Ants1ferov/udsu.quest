b9b72277993b503fb1fb6e545fdd00f6099c8ccc
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-27 02:03:48 +0400

    update App
    добавил анимации при переходе между страницами

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <RouterView />
-  </div>
+  <router-view v-slot="{ Component }">
+    <transition name="question">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style scoped>
