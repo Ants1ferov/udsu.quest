ecace438678ff888567069e741ee76b26fb85edc
Vitaliy <107627295+vgxot@users.noreply.github.com>
2023-03-28 12:50:07 +0400

    extensive update quest 1

---
+++ src/components/UI/fastPopUp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fast-pop-up">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "fast-pop-up"
+}
+</script>
+
+<style scoped>
+.fast-pop-up{
+  display: flex;
+  align-items: center;
+  position: fixed;
+  width: 70%;
+  height: 200px;
+  padding: 20px;
+  background-color: #1e1e1e;
+  bottom: 300px;
+  border-radius: 25px;
+  z-index: 15;
+}
+</style>
